5edac2e5e194c0bfefd3b8be7cb6d0e9926e58e8
lai95 <mgrivas95@gmail.com>
2026-03-19 14:25:29 +0200

    first commit

---
+++ testservergo.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type Heartbeat struct {
+	Hostname  string `json:"hostname"`
+	Timestamp string `json:"timestamp"`
+	Status    string `json:"status"`
+}
+
+func heartbeatHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	var hb Heartbeat
+	if err := json.NewDecoder(r.Body).Decode(&hb); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	log.Printf("heartbeat: hostname=%s timestamp=%s status=%s", hb.Hostname, hb.Timestamp, hb.Status)
+	w.WriteHeader(http.StatusOK)
+}
+
+func main() {
+	http.HandleFunc("/heartbeat", heartbeatHandler)
+	log.Println("server listening on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
